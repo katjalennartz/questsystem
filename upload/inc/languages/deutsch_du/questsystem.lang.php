bfc616f6d29022a799a9ba3df33e4147dd4f148b
Katja <47885564+katjalennartz@users.noreply.github.com>
2024-10-16 18:55:14 +0200

    Update questsystem.lang.php

--- upload/inc/languages/deutsch_du/questsystem.lang.php
+++ upload/inc/languages/deutsch_du/questsystem.lang.php
@@ -2,6 +2,25 @@
 
 $l['questsystem_name'] = "Questsystem";
 
+$l['questsystem_timeperiode_days'] = "<span><b>Zeitraum:</b> {1} Tage</span>";
+$l['questsystem_timeperiode_none'] = "<span><b>Zeitraum:</b> Keine Begrenzung</span>";
+
+$l['questsystem_admin_allocation'] = "<span><b>Quest wird von einem Admin zugeteilt.</b></span>";
+$l['questsystem_admin_allocation_none'] = "<span><b>Quest wird zufällig gezogen.</b></span>";
+
+$l['questsystem_points_for_quest'] = "<span><b>Punkte bei Erfolg:</b> {1} Punkte</span>";
+$l['questsystem_points_for_quest_minus'] = "<span><b>Punkte bei Erfolg:</b> {1} Punkte</span>";
+
+$l['questsystem_end_post'] = "<span>Quest wird mit einem Post beendet.</span>";
+$l['questsystem_end_scene'] = "<span>Quest wird mit einer Szene beendet.</span>";
+
+$l['questsystem_submitted'] = "<span><b>Quest wurde eingereicht.</b></span>";
+
+$l['questsystem_waiting'] = "<span>wartet auf Zuteilung</span>";
+
+$l['questsystem_daysuntil'] = "<span><b>Bis zum:</b> {1}</span>";
+$l['questsystem_daysuntil_none'] = "<span><b>Bis zum:</b> Keine Begrenzung</span>";
+
 //alerts
 $l['myalerts_setting_questsystem_QuestAccepted'] = 'Questsystem: Benachrichtigung wenn ein Questvorschlag akzeptiert wurde?';
 $l['questsystem_QuestAccepted'] = '{1} dein Questvorschlag wurde akzeptiert und du hast die Punkte bekommen.';
@@ -11,4 +30,3 @@ $l['questsystem_giveUser'] = 'Dir wurde ein Quest zugeteilt. Schaue in der Quest
 
 $l['myalerts_setting_questsystem_QuestDeny'] = 'Questsystem: Benachrichtigung wenn ein eingereichtes Quest nicht akzeptiert wurde?';
 $l['questsystem_QuestDeny'] = 'Deine Questeinreichung/Aufgabe wurde nicht akzeptiert und du musst das Quest erneut bearbeiten. Bei Fragen schreib einem Teammitglied.';
-
