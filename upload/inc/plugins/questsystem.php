9a70cac5fdc61ba9b7b79d701b8ef1bbe8b9da0f
Katja <47885564+katjalennartz@users.noreply.github.com>
2025-03-05 15:31:57 +0100

    Update questsystem.php

--- upload/inc/plugins/questsystem.php
+++ upload/inc/plugins/questsystem.php
@@ -11,8 +11,8 @@
  * 
  */
 // enable for Debugging:
-error_reporting(E_ERROR | E_PARSE);
-ini_set('display_errors', true);
+//error_reporting(E_ERROR | E_PARSE);
+//ini_set('display_errors', true);
 
 
 
